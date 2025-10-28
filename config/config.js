428741a7a27b1a6674f95097b1fe35cc768a0add
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2025-10-28 14:36:09 +0700

    setup express.js

---
+++ config/config.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const development = {
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_NAME,
+  "host": process.env.DB_HOST, 
+  "dialect" : process.env.DB_DIALECT
+}; 
+
+const test = {
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_NAME,
+  "host": process.env.DB_HOST,
+  "dialect": process.env.DB_DIALECT
+};
+
+const production = {
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_NAME,
+  "host": process.env.DB_HOST,
+  "dialect": process.env.DB_DIALECT
+};
+
+module.exports = {
+  development, 
+  test,
+  production
+};
